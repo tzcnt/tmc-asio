de91191bb2082ddcb5a6c548cc10156e1cb5db19
tzcnt <trailingzerocount@gmail.com>
2023-09-16 22:10:29 -0700

    initial commit

---
+++ tmc/include/asio/aw_asio.hpp
@@ -0,0 +1,140 @@
+#pragma once
+#include "asio/async_result.hpp"
+#include "tmc/detail/thread_locals.hpp"
+#include <functional>
+#include <optional>
+#include <string>
+#include <thread>
+namespace tmc {
+
+template <typename... Args> struct aw_asio_base {
+  std::optional<std::tuple<Args...>> result;
+  std::coroutine_handle<> outer;
+  detail::type_erased_executor *continuation_executor;
+  size_t prio;
+
+  struct callback {
+    aw_asio_base *me;
+    template <typename... Args_> void operator()(Args_ &&...args) {
+      me->result.emplace(std::move(args)...);
+      if (me->continuation_executor == detail::this_thread::executor) {
+        me->outer.resume();
+      } else {
+        me->continuation_executor->post_variant(std::move(me->outer), me->prio);
+      }
+    }
+  };
+
+  virtual void initiate(callback cb) = 0;
+  virtual ~aw_asio_base() = default;
+
+  aw_asio_base()
+      : continuation_executor(detail::this_thread::executor),
+        prio(detail::this_thread::this_task.prio) {}
+  aw_asio_base(aw_asio_base &&other) : result(std::move(other.result)) {}
+
+  bool await_ready() { return false; }
+
+  void await_suspend(std::coroutine_handle<> h) noexcept {
+    outer = h;
+    initiate(callback{this});
+  }
+
+  auto await_resume() noexcept { return *std::move(result); }
+
+  inline aw_asio_base &resume_on(detail::type_erased_executor *executor) {
+    continuation_executor = executor;
+    return *this;
+  }
+  template <detail::TypeErasableExecutor Exec>
+  aw_asio_base &resume_on(Exec &executor) {
+    return resume_on(executor.type_erased());
+  }
+  template <detail::TypeErasableExecutor Exec>
+  aw_asio_base &resume_on(Exec *executor) {
+    return resume_on(executor->type_erased());
+  }
+};
+
+struct aw_asio_t {
+  constexpr aw_asio_t() {}
+
+  // Adapts an executor to add the `aw_asio_t` completion token as the default.
+  template <typename InnerExecutor>
+  struct executor_with_default : InnerExecutor {
+    typedef aw_asio_t default_completion_token_type;
+
+    executor_with_default(const InnerExecutor &ex) noexcept
+        : InnerExecutor(ex) {}
+
+    template <typename InnerExecutor1>
+    executor_with_default(
+        const InnerExecutor1 &ex,
+        typename std::enable_if<std::conditional<
+            !std::is_same<InnerExecutor1, executor_with_default>::value,
+            std::is_convertible<InnerExecutor1, InnerExecutor>,
+            std::false_type>::type::value>::type = 0) noexcept
+        : InnerExecutor(ex) {}
+  };
+
+  // Type alias to adapt an I/O object to use `aw_asio_t` as its
+  // default completion token type.
+  template <typename T>
+  using as_default_on_t = typename T::template rebind_executor<
+      executor_with_default<typename T::executor_type>>::other;
+
+  // Function helper to adapt an I/O object to use `aw_asio_t` as its
+  // default completion token type.
+  template <typename T>
+  static typename std::decay_t<T>::template rebind_executor<
+      executor_with_default<typename std::decay_t<T>::executor_type>>::other
+  as_default_on(T &&object) {
+    return typename std::decay_t<T>::template rebind_executor<
+        executor_with_default<typename std::decay_t<T>::executor_type>>::
+        other(std::forward<T>(object));
+  }
+};
+
+// Static completion token object that tells asio to produce a TMC awaitable.
+constexpr aw_asio_t aw_asio{};
+
+} // namespace tmc
+
+#ifdef TMC_USE_BOOST_ASIO
+namespace boost::asio {
+#else
+namespace asio {
+#endif
+
+// Specialization of asio::async_result to produce a TMC awaitable
+template <typename... Args> struct async_result<tmc::aw_asio_t, void(Args...)> {
+  template <typename Initiation, typename... InitArgs>
+  struct aw_asio final : tmc::aw_asio_base<std::decay_t<Args>...> {
+    Initiation initiation;
+    std::tuple<InitArgs...> init_args;
+    template <typename Initiation_, typename... InitArgs_>
+    aw_asio(Initiation_ &&initiation, InitArgs_ &&...args)
+        : initiation(std::forward<Initiation_>(initiation)),
+          init_args(std::forward<InitArgs_>(args)...) {}
+
+    void initiate(
+        tmc::aw_asio_base<std::decay_t<Args>...>::callback cb) final override {
+      std::apply(
+          [&](InitArgs &&...init_args) {
+            std::move(initiation)(std::move(cb), std::move(init_args)...);
+          },
+          std::move(init_args));
+    }
+  };
+
+  // This doesn't actually initiate the operation, just returns the awaitable.
+  // Initiation happens in aw_asio_base::await_suspend();
+  template <typename Initiation, typename... InitArgs>
+  static aw_asio<std::decay_t<Initiation>, std::decay_t<InitArgs>...>
+  initiate(Initiation &&initiation, tmc::aw_asio_t, InitArgs &&...args) {
+    return aw_asio<std::decay_t<Initiation>, std::decay_t<InitArgs>...>(
+        std::forward<Initiation>(initiation), std::forward<InitArgs>(args)...);
+  }
+};
+
+} // namespace asio
---
+++ tmc/include/asio/ex_asio.hpp
@@ -0,0 +1,113 @@
+#pragma once
+#include "asio.hpp"
+#include "tmc/detail/thread_locals.hpp"
+#include <functional>
+#include <string>
+#include <thread>
+namespace tmc {
+struct ex_asio {
+#ifdef TMC_USE_BOOST_ASIO
+  using ioc_t = boost::asio::io_context;
+#else
+  using ioc_t = asio::io_context;
+#endif
+  ioc_t ioc;
+  std::jthread ioc_thread;
+  tmc::detail::type_erased_executor type_erased_this;
+  bool is_initialized;
+  inline void init(int nthreads = 1) {
+    if (is_initialized) {
+      return;
+    }
+    is_initialized = true;
+    if (ioc.stopped()) {
+      ioc.restart();
+    }
+    ioc.get_executor()
+        .on_work_started(); // replaces need for an executor_work_guard
+    ioc_thread = std::jthread([this]() {
+      init_thread_locals(0);
+      ioc.run();
+    });
+  }
+  inline void teardown() {
+    if (!is_initialized) {
+      return;
+    }
+    is_initialized = false;
+    ioc.get_executor()
+        .on_work_finished(); // replaces need for an executor_work_guard
+    ioc.stop();
+    ioc_thread.join();
+  }
+
+  inline ex_asio() : ioc(1), type_erased_this(*this), is_initialized(false) {}
+  inline ex_asio(int nthreads)
+      : ioc(nthreads), type_erased_this(*this), is_initialized(false) {
+    init(nthreads);
+  }
+  inline ~ex_asio() { teardown(); }
+  inline tmc::detail::type_erased_executor *type_erased() {
+    return &type_erased_this;
+  }
+  inline void init_thread_locals(size_t slot) {
+    detail::this_thread::executor = &type_erased_this;
+    // detail::this_thread::this_task = {.prio = 0, .yield_priority =
+    // &yield_priority[slot]};
+    // use string concatenation to avoid needing add'l headers
+    detail::this_thread::thread_name =
+        std::string("i/o thread ") + std::to_string(slot);
+  }
+
+  inline void clear_thread_locals() {
+    detail::this_thread::executor = nullptr;
+    // detail::this_thread::this_task = {};
+    detail::this_thread::thread_name.clear();
+  }
+  inline void graceful_stop() { ioc.stop(); }
+
+  inline void post_variant(work_item &&item, size_t priority) {
+#ifdef TMC_USE_BOOST_ASIO
+    boost::asio::post(ioc.get_executor(), item);
+#else
+    asio::post(ioc.get_executor(), item);
+#endif
+  }
+
+  template <typename It>
+  void post_bulk(It items, size_t priority, size_t count) {
+    for (size_t i = 0; i < count; ++i) {
+#ifdef TMC_USE_BOOST_ASIO
+      boost::asio::post(ioc.get_executor(), *items);
+#else
+      asio::post(ioc.get_executor(), *items);
+#endif
+      ++items;
+    }
+  }
+
+  // Make it possible to pass this directly to asio functions,
+  // Whether you are using the default type-erased any_io_executor
+  // or the specialized io_context executor
+  using executor_type = ioc_t::executor_type;
+  inline operator executor_type() { return ioc.get_executor(); }
+  inline operator asio::any_io_executor() { return ioc.get_executor(); }
+};
+
+namespace detail {
+#ifndef TMC_NO_GLOBAL_RUNTIME
+inline ex_asio g_ex_asio;
+#else
+inline ex_asio *g_ex_asio;
+#endif
+} // namespace detail
+
+constexpr ex_asio &asio_executor() {
+#ifndef TMC_NO_GLOBAL_RUNTIME
+  return detail::g_ex_asio;
+#else
+  return *detail::g_ex_asio;
+#endif
+}
+
+} // namespace tmc
