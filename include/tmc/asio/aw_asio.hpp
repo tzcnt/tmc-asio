07cd9ac0daba9187ca1a28e3e1b7d591bf498ede
tzcnt <trailingzerocount@gmail.com>
2026-01-06 21:21:55 -0800

    update copyright notice year

--- include/tmc/asio/aw_asio.hpp
+++ include/tmc/asio/aw_asio.hpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2022 Klemens Morgenstern (klemens.morgenstern@gmx.net)
-// Copyright (c) 2023-2025 Logan McDougall
+// Copyright (c) 2023-2026 Logan McDougall
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
